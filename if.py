1fba185ec2ff53bd7fadc736d77042e80fe11e6d
watchdog20 <jkutniewski03@gmail.com>
2014-09-05 20:03:19 -0400

    made the if statements and elif and else

---
+++ if.py
@@ -0,0 +1,11 @@
+print("are you a d- student")
+answer = input("<")
+
+if answer == 'yes':
+    print ("ok i wont tell anybody that")
+elif answer == 'no':
+    print("oh so what are you")
+elif answer == 'well i dident care any way':
+    print("ho cares a bout you")
+else:
+    print("ok so you are a c- a b - or are you a a- student.")
